ded9fd6dad25287a7e1fd6edb001813f931de5d8
Tanushree padole <tanushreepadole888@gmail.com>
2026-08-08 11:11:13 +0530

    Time: 1721 ms (5.01%), Space: 28.1 MB (93.86%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+   for (int i = 0; i < nums.size(); i++){
+    int count = 1;
+      for (int j = i + 1; j < nums.size(); j++){
+         if(nums [i] == nums[j]){
+
+      
+                  count++;
+             }
+        }     
+   if(count > nums.size()/2)
+    {
+        return nums[i];
+    }
+    
+        }
+        return -1;
+    }
+};
