4c796b89187c674a1dae3cbacf65c7175958b54e
Tanushree padole <tanushreepadole888@gmail.com>
2026-09-05 23:54:09 +0530

    Time: 0 ms (100%), Space: 21.9 MB (12.05%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+       int n = nums.size();
+       int expectedSum = n * (n + 1) / 2;
+       int actualSum = 0; 
+
+        for (int i = 0; i < nums.size(); i++){
+            actualSum = actualSum + nums[i];
+        }
+      
+
+      int ans = expectedSum - actualSum;
+      return ans;
+
+
+
+
+
+    }
+};
