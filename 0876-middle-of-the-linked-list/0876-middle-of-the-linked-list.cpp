963c52ff64da21d328941aaa4fb6fedd06990120
Tanushree padole <tanushreepadole888@gmail.com>
2026-09-11 23:59:05 +0530

    Time: 0 ms (100%), Space: 9.9 MB (64.4%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+   ListNode* i = head;
+ListNode* j = head;
+  
+   while (j != NULL && j->next != NULL){
+
+     i = i->next;
+    j = j->next->next;
+   }
+  
+  return i;
+
+
+
+
+
+
+
+
+    }
+};
